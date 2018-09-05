db95bb0a14bb23f92372be2eaf870edacca5bd0e
Hamil1 <hamil151@hotmail.com>
2018-09-04 22:33:55 -0400

    commit

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const cpu = require('os');
 const lib = require('./lib.js');
+const request = require('request');
+const https = require('https');
 
-let pc = cpu.cpus();
-let resultado = lib.sumarDosNumeros(45,2);
+let CHROME_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
 
-console.log(resultado);
+let options = {
+    host: 'api.github.com',
+    path: '/users/' + username,
+    method: 'GET',
+    headers: {'user-agent': CHROME_USER_AGENT}
+}
+
+https.request(options, function(response){
+    let body;
+    response.on('data', (out) => {
+        body += out;
+    });
+});
+
+console.log(https);
 debugger;
