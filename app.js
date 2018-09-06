f62b7b97a717f8fd7ededcfb7de44e4c537da3c8
Hamil <victor-diaz@1-PC-TI-0121-00.Optic.local>
2018-09-06 17:07:52 -0400

    + express

--- app.js
+++ app.js
@@ -2,31 +2,12 @@ const cpu = require('os');
 const lib = require('./lib.js');
 const request = require('request');
 const https = require('https');
+const fetch = require('node-fetch');
 
-let username = "Hamil1";
-let CHROME_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
-
-let options = {
-    host: 'api.github.com',
-    path: '/users/' + username,
-    method: 'GET',
-    headers: {'user-agent': CHROME_USER_AGENT}
-}
-
-let consulta = https.request(options, function(response){
-    let body = '';
-    response.on('data', (out) => {
-        body += out;    
-    });
-
-    response.on('end',function(out){
-        let json = JSON.parse(body);
-        console.log(json);
-    });
+fetch('https://api.github.com/users/Hamil1').then(function(data){
+    console.log(data);
+    return data.json();
+}).then(function(json){
+    console.log('Este es el resultado de la consulta: ' + JSON.stringify(json));
+    debugger;
 });
-
-consulta.on('error', function(error){
-    console.log('Este es el error: ' + error);
-});
-
-consulta.end();
