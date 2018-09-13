35d6da7a6d4b1b5cc6b06d31cd2e9458bb875c4d
Hamil <hamil151@hotmail.com>
2018-09-13 10:15:35 -0400

    borrando todo para usar express generator

--- lib.js
+++
@@ -1,8 +0,0 @@
-function sumarDosNumeros(primerNumero, segundoNumero){
-    let resultado = primerNumero + segundoNumero;
-    return resultado;
-}
-
-module.exports = {
-    sumarDosNumeros: sumarDosNumeros,
-}
